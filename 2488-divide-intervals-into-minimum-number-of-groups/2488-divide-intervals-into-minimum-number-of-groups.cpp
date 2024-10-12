a66051aa335c61203ea11e6109feed320a41eb22
coder55 <140712979+JUTURUHARSHITHA@users.noreply.github.com>
2024-10-12 11:29:38 +0530

    Time: 245 ms (88.06%), Space: 96.9 MB (67.79%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -8,7 +8,6 @@ public:
         }
         sort(start_times.begin(), start_times.end());
         sort(end_times.begin(), end_times.end());
-
         int end_ptr = 0, group_count = 0;
         for (int start : start_times) {
             if (start > end_times[end_ptr]) {
